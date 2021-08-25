5082a412f5fcc829ec019a656b82b9e410c5b83b
Cesar Nebot <cnebot@um.es>
2021-08-25 20:20:31 +0200

    Function Outlier Treatment version 1

--- OutlierTreatment.R
+++ OutlierTreatment.R
@@ -1,93 +1,114 @@
+#==============================================================#
+# Generic program for outlier detection in Time series based on  
+# Structural Change Chow test detection, stationary subseries 
+# y must be a 
+#==============================================================#
 
-
+# 1. SETUP  
 library(rstudioapi)
 rstudioapi::getActiveDocumentContext
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+options(warn=-1)
 
 
-OutlierTreatment<- function(y,ld=5.rad=3){
+
+# 2. Packages
+
+library(strucchange)   # install.packages("strucchange")
+library(car)           # install.packages("car")
+library(tseries)       # install.packages("tseries")
+
+
+OutlierTreatment<- function(y,freq=12,start.date,level.detection=5,radius=2){
   
-#Generic program for outlier detection in Time series based on  
-#Structural Change Chow test detection, stationary subseries 
+  
+if(!is.ts(y)) y<-ts(y,frequency=freq, start=start.date)  
+  
+  
+# Parameters
+ld<-level.detection      # Cook's distance to determine Outlier 
+r<-radius                   # radius for mean around outlier position for substitution 
 
-#Parameters
-level.detection=ld
-r=rad  # radius for mean around outlier position for substitution 
- # which column of data is the time series to be considered 
-
- 
- #Loading data
-#input data file to be clean Data.csv 
- Data.OUT<-read.csv("Data.csv", header = T,sep=";",dec=".")
-
- name<-colnames(Data.OUT)[column]
+# Characteristics of the series
+l<-length(y)
+plot.ts(y)
 
 
- l<-length(Data.OUT[,column])
- y<-Data.OUT[,column]
- plot.ts(y)
-
-#packages
-#install.packages("strucchange")
-library(strucchange)
-#install.packages("car")
-library(car)
-#install.packages("tseries")
-library(tseries)
-
-# Looks for breakpoints
+# Looking for breakpoints for stationary segments
 
 bp.y <- breakpoints(y ~ 1)
 summary(bp.y)
+breakpoints(bp.y)
 split<-bp.y$breakpoints
+breakdates(bp.y)
+splitd<-breakdates(bp.y)
+ci.y <- confint(bp.y)
+
+
 ls<- length(split)
 
-# Split into stationary subseries
+fm0 <- lm(y ~ 1)
+fm1 <- lm(y ~ breakfactor(bp.y, breaks = length(split)))
+plot.ts(y)
+lines(ts(fitted(fm0), start=c(2008,1),frequency=12), col = 3)
+lines(ts(fitted(fm1), start=c(2008,1), frequency = 12), col = 4)
+
+## confidence interval
+ci.y <- breakdates(confint(bp.y))
+for(k in 1:nrow(ci.y)){
+abline(v=ci.y[k,], lty=c(2,1,2), col=c(2),lwd=c(1,2,1))
+}
+
+
+# Split into subseries
 
 split.components<-c(1,split,l+1)
 
 par(mfrow=c(2,2))
 plot.ts(y,main="All y")
+
+
 for(i in 1:I(ls+1)){
   yy<-y[split.components[i]:I(split.components[i+1]-1)]
   nam <- paste("yy", i, sep = "")
   assign(nam, yy)
 
   plot.ts(yy,main = paste("Split y", i, sep = ""))
-
+ 
   #Augmented Dickey Fuller test
-  nam.df <- paste("adf.yy", i, sep = "")
+  nam.df <- paste("adf.yy", i, sep ="")
   k<-round((length(yy)-1)^(1/3))
   assign(nam.df, adf.test(yy,k=k))
-
 }
 
- ## Print Augmented Dickey fuller test for subseries 
+## Print Augmented Dickey fuller test for subseries 
 for(i in 1:I(ls+1)){
   nam.df <- paste("adf.yy", i, sep = "")
-  print(get( nam.df))
+  print(get(nam.df))
 }
 
 par(mfrow=c(1,1))
 
 
 # Outlier detection by subseries and substitute in ynew the mean value 
-
-
 ynew<-y
 
+length(c(yy1))+ length(c(yy2))
+length(c(yy3))
 
-for ( i in 1:I(ls+1)){
+for (i in 1:I(ls+1)){
 subseries<-get(paste("yy",i,sep=""))
 
 mod<-lm(subseries~1)
 outlierTest(mod)
 cooksd1 <- cooks.distance(mod)
 plot(cooksd1, pch="·", cex=2, main="Influential Obs by Cooks distance")  # plot cook's distance
-abline(h = level.detection*mean(cooksd1, na.rm=T), col="red")  # add cutoff line
-text(x=1:length(cooksd1)+1, y=cooksd1, labels=ifelse(cooksd1>level.detection*mean(cooksd1, na.rm=T),names(cooksd1),""), col="red")  # add labels
+abline(h = ld*mean(cooksd1, na.rm=T), col="red")  # add cutoff line
+text(x=1:length(cooksd1)+1, y=cooksd1, 
+     labels=ifelse(cooksd1>ld*mean(cooksd1, na.rm=T),names(cooksd1),""), 
+     col="red")  # add labels
 
-outliers1 <- cooksd1[cooksd1>I(level.detection*mean(cooksd1, na.rm=T))]
+outliers1 <- cooksd1[cooksd1>I(ld*mean(cooksd1, na.rm=T))]
 outlier.pos1<-as.vector(as.numeric(names(outliers1)))
 
 if(length(outliers1)!=0){
@@ -97,20 +118,17 @@ if(length(outliers1)!=0){
     ball<-y[I(j-r):I(j+r)]
     numball<-2*r-(sum(is.na(ball)))
     ynew[j]<-(sum(ball,na.rm=T)-y[j])/(numball)
+    }
   }
- }
 
 
 }
 
 par(mfrow=c(1,1))
-plot.ts(y,ylim=c(0,max(y)))
-plot.ts(ynew,ylim=c(0,max(y)))
-
-file.name=paste("new",name,"(",level.detection,").csv",sep="")
-
+plot<-ts.plot(y,ynew, ylim=c(0,max(y)),col=c(2,4),lty=c(1,2))
 changes<-1-(y==ynew) 
 result<-cbind(y,ynew,changes )
 colnames<-c("y","ynew", "changed")
-write.table(result, file.name,col.names=TRUE,dec=".",sep=";",row.names = FALSE)
 
+return(result)
+}
